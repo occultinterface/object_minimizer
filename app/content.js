0b15f80e84fc36f8608383b90b183696f016a944
Julia Both <occultinterface@gmail.com>
2020-01-29 17:51:44 +1100

    dark mode button

--- app/content.js
+++ app/content.js
@@ -2,11 +2,10 @@ console.log(' it works');
 const answers = document.querySelectorAll('nav>button');
 const words = document.querySelector('#main-text');
 const theme = document.querySelector('.button--switch-theme');
-const container = document.querySelector('.container');
+const container = document.querySelector('body');
 
 const themeSwitcher = function () {
-    const variable = 'red';
-    container.setAttribute('style', `--background: ${variable}`);
+    container.classList.toggle('dark');
     console.log('switch theme')
 };
 
@@ -21,10 +20,10 @@ const questions = [
 ]
 const eliminators = [
     'Picture yourself writing this object into your will. If you die tomorrow, would you want someone you know to keep this?',
-    'If this object is so dear, are you willing to carry it until the day of your dying, even if you like another 100 years?',
+    'If this object is so dear, are you willing to carry it until the day of your dying, even if you live another 100 years?',
 ];
 
-const conclusions = ['Let it gox.', 'Keep it.', ];
+const conclusions = ['Let it go.', 'Keep it.', ];
 
 const arrays = [questions, eliminators, conclusions];
 
@@ -38,12 +37,21 @@ words.textContent = `${display}`;
 
 function handleAnswer(e) {
     const answer = e.currentTarget.id;
-    let lastArray = currentArray;
-    let yesLimit = lastArray.length - 1;
+    
+    let yesLimit = currentArray.length - 1;
+    // if  (answer === 'back'){
+    //     switch(e){
+    //     case index < currentArray.length:
+    //         null;
+    //         break;
+    //     case: 
+    //     default:
+    //         break;
+    //     }
+        
+    // }
 
-    if (answer === 'back') {
-        console.log('back')
-    } else if (answer === 'yes' && index < yesLimit) {
+    if (answer === 'yes' && index < yesLimit) {
         index++;
     } else if (answer === 'no') {
         index = 0;
@@ -52,18 +60,11 @@ function handleAnswer(e) {
         index = 1;
         arrayIndex = 2;
     }
+    
+    //Update the variables above
     currentArray = arrays[arrayIndex];
     display = currentArray[index];
-    words.textContent = `
-                $ {
-                    display
-                }
-                `;
-    // console.log(`ArrayIndex: ${arrayIndex}`);
-    // console.log(`index: ${index}`);
-    // console.log(currentArray);
-    // console.log(currentArray[index]);
-    // console.log(`-- -- -- -- --`);
+    words.textContent = `${display}`;
 }
 handleClick = function (e) {
     e.addEventListener('click', handleAnswer);
